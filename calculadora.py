1dff2d6f6ba043c948fec05607c77b32db3b08fc
victorpellicci <victor1994@hotmail.com.br>
2021-10-21 14:45:33 -0300

    1.0

--- calculadora.py
+++ calculadora.py
@@ -20,6 +20,8 @@ class CalculadoraApp:
         self.entry3 = builder.get_object('entry3')
         self.label4 = builder.get_object('label4')
         self.label5 = builder.get_object('label5')
+        self.checkbutton1 = builder.get_object('checkbutton1')
+        print(self.checkbutton1.state())
         self.dado = "asdfg"
         data = '123455'
         print(data)
@@ -27,59 +29,92 @@ class CalculadoraApp:
     def run(self):
         self.mainwindow.mainloop()
 
-    def adicao(self):
+    def adicaoerro(self):
         caixa1 = self.entry1.get()
         caixa2 = self.entry2.get()
+        try:
+            return(float(caixa1)+float(caixa2))
+        except ValueError:
+            return('Use "." no lugar de ","')
+
+    def adicao(self):
+        if (self.checkbutton1.state()[0]=='alternate' or self.checkbutton1.state()[0]==''):
+            self.entry1.delete(0, 'end')
+            self.entry2.delete(0, 'end')
         self.entry3.delete(0, 'end')
-        self.entry3.insert(0,float(caixa1)+float(caixa2))
+        self.entry3.insert(0,self.adicaoerro())
+
+    def subtracaoerro (self):
+        caixa1 = self.entry1.get()
+        caixa2 = self.entry2.get()
+        try:
+            return(float(caixa1)-float(caixa2))
+        except ValueError:
+            return('Use "." no lugar de ","')
 
     def subtracao(self):
+        self.entry3.delete(0, 'end')
+        self.entry3.insert(0,self.subtracaoerro())
+
+    def multiplicacaoerro (self):
         caixa1 = self.entry1.get()
         caixa2 = self.entry2.get()
-        self.entry3.delete(0, 'end')
-        self.entry3.insert(0,float(caixa1)-float(caixa2))
+        try:
+            return(float(caixa1)*float(caixa2))
+        except ValueError:
+            return('Use "." no lugar de ","')
 
     def multiplicacao(self):
-        caixa1 = self.entry1.get()
-        caixa2 = self.entry2.get()
         self.entry3.delete(0, 'end')
-        self.entry3.insert(0,float(caixa1)*float(caixa2))
+        self.entry3.insert(0,self.multiplicacaoerro())
 
-    def texto_erro(self): 
+    def divisaoerro(self): 
         caixa1 = self.entry1.get()
         caixa2 = self.entry2.get()
-        if (caixa2==0):
-            return('Erro! Divisão por zero!')
-        else:   
+        try:
             return(float(caixa1)/float(caixa2))
+        except ZeroDivisionError:   
+            return('Erro! Divisão por zero!')
+        except ValueError:
+            return('Use "." no lugar de ","')
             
     def divisao(self):
-        resultado = self.texto_erro()
-
         self.entry3.delete(0, 'end')
-        self.entry3.insert(0, resultado)
+        self.entry3.insert(0,self.divisaoerro())
+
+    def potenciacaoerro(self):
+        caixa1 = self.entry1.get()
+        caixa2 = self.entry2.get()
+        try:
+            return(float(caixa1)**float(caixa2))
+        except ValueError:
+            return('Use "." no lugar de ","')
 
     def potenciacao(self):
+        self.entry3.delete(0, 'end')
+        self.entry3.insert(0,self.potenciacaoerro())
+
+    def radiciacaoerro(self):
         caixa1 = self.entry1.get()
         caixa2 = self.entry2.get()
-        self.entry3.delete(0, 'end')
-        self.entry3.insert(0,float(caixa1)**float(caixa2))
+        try:
+            return(float(caixa1)**(1/float(caixa2)))
+        except ValueError:
+            return('Use "." no lugar de ","')
 
     def radiciacao(self):
-        caixa1 = self.entry1.get()
-        caixa2 = self.entry2.get()
         self.entry3.delete(0, 'end')
-        self.entry3.insert(0,float(caixa1)**(1/float(caixa2)))
+        self.entry3.insert(0,self.radiciacaoerro())
         self.label4.config(text='1º numero: Radicando')
         self.label5.config(text='2º numero: Indice')
 
+    def manter(self):
+        print(self.checkbutton1.state())
+
 if __name__ == '__main__':
-
-
 
     app = CalculadoraApp()
     print(app.dado)
-    #print(app.data)
 
     builder.connect_callbacks(CalculadoraApp())
     app.run()
