12606663edd260504769920f213ca5319cfd1ce9
victorpellicci <victor1994@hotmail.com.br>
2021-10-27 10:45:15 -0300

    1.0

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from cx_Freeze import setup, executable
 base=None
 if platform == 'win32':
 	base='Win32Gui'
+bild_options = {	
+		'includes':['tkinter', 'ttkbootstrap', ]}
 
 setup(name='Calculadora do Victor', 
 	version='1.0',
 	description='Calculadora rapida para o dia a dia',
-	options={'build_exe': {	
-		'includes':['tkinter', 'ttkbootstrap', ]}},
-	executables=[executable('calculadora.py', base=base)])
+	options={'build_exe':bild_options}, 
+	executables=[Executable('main.py', base=platform)])
